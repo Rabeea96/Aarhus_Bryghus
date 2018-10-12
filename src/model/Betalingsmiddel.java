aadfc492ebd891bebfefc3f7ae6c0b956a28539c
DESKTOP-K4VE6AB\rabee <Rabeea96@hotmail.com>
2018-10-12 14:14:04 +0200

    jkn4

--- src/model/Betalingsmiddel.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Betalingsmiddel {
-    DANKORT, KONTANT, KLIPPEKORT, MOBILEPAY, REGNING;
-}
--- src/model/Strategy_giv_rabat.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface Strategy_giv_rabat {
-
-    public double giv_rabat(double rabat, Ordre ordre);
-
-}
